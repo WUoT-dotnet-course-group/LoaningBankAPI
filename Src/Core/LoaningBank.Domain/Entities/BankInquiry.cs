90859b4492341183b8ee5282d7a643145c1443d0
Joanna Sowa <joannasovva@gmail.com>
2022-11-10 20:10:04 +0100

    Classes related to bank inquiries added.

---
+++ Src/Core/LoaningBank.Domain/Entities/BankInquiry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoaningBank.Domain.Entities
+{
+    internal class BankInquiry : Inquiry
+    {
+        public Guid? OfferId { get; set; }
+    }
+}
---
+++ Src/Core/LoaningBank.Domain/Entities/Offer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoaningBank.Domain.Entities
+{
+    public class Offer
+    {
+        public Guid Id { get; set; }
+        public OfferStatus Status { get; set; }
+    }
+}
