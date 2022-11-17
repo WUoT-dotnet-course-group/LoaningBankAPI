40bb5be5e02c9070eeeb7dad3959be09e355ed89
Jakub Urbański <kuba.urban01@gmail.com>
2022-11-17 01:34:54 +0100

    add and configure inquiries and offers db sets

--- Src/Infrastructure/LoaningBank.DataPersistence/RepositoryDbContext.cs
+++ Src/Infrastructure/LoaningBank.DataPersistence/RepositoryDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LoaningBank.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoaningBank.DataPersistence
 {
@@ -7,6 +8,23 @@ namespace LoaningBank.DataPersistence
         public RepositoryDbContext(DbContextOptions<RepositoryDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Inquiry> Inquiries { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inquiry>()
+                .OwnsOne(
+                x => x.PersonalData,
+                pd =>
+                {
+                    pd.Property(y => y.FirstName).HasColumnName("DebtorFirstName");
+                    pd.Property(y => y.LastName).HasColumnName("DebtorLastName");
+                    pd.Property(y => y.GovernmentId).HasColumnName("DebtorGovernmentId");
+                    pd.Property(y => y.GovernmentIdType).HasColumnName("DebtorGovernmentIdType");
+                });
         }
     }
 }
