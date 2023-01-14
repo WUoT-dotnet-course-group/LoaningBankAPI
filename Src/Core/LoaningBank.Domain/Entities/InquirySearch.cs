bed68fb50ce6ba72f36961f29966331757295075
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-14 08:25:01 +0100

    LOAN-43: add InquirySearch view

---
+++ Src/Core/LoaningBank.Domain/Entities/InquirySearch.cs
@@ -0,0 +1,18 @@
+﻿using LoaningBank.CrossCutting.Enums;
+
+namespace LoaningBank.Domain.Entities
+{
+    public class InquirySearch
+    {
+        public Guid InquiryID { get; set; }
+        public Guid? OfferID { get; set; }
+        public int LoanValue { get; set; }
+        public short NumberOfInstallments { get; set; }
+        public DateTime InquireDate { get; set; }
+        public OfferStatus? Status { get; set; }
+        public float? Percentage { get; set; }
+        public DateTime? OfferCreateDate { get; set; }
+        public DateTime? OfferUpdateDate { get; set; }
+        public string? ApprovedBy { get; set; }
+    }
+}
